18c60974c3d6ef566d09079898a94561c950d409
Eric Davenport <EricDavenport@pursuit.org>
2020-04-14 12:28:20 -0400

    created Core Data mediaObject model entity and attributes - generated managed class

---
+++ AVFoundation-MediaFeed/CDMediaObject+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  CDMediaObject+CoreDataClass.swift
+//  AVFoundation-MediaFeed
+//
+//  Created by Eric Davenport on 4/14/20.
+//  Copyright © 2020 Eric Davenport. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class CDMediaObject: NSManagedObject {
+
+}
